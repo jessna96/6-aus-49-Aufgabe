a4eca9478575bc1026cc366b5ffa09045c739038
jessna96 <35634254+jessna96@users.noreply.github.com>
2023-02-04 17:38:40 +0100

    Remove comments

--- lotteryAPI/Program.cs
+++ lotteryAPI/Program.cs
@@ -7,17 +7,12 @@ using MySql.Data.MySqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddDbContext<DataContext>(options => options.UseMySQL(Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
